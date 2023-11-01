8e1691f68269122e3c2a3a169675b86721c9af3c
AlexKorablik <110320085+AlexKorablik@users.noreply.github.com>
2023-11-01 20:52:39 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
           </span>
           <span className="secondaryText">
             <a
-              href="https://drive.google.com/file/d/12mfsQEo8bfd41IuZ9N6eadmdsUlzut3g/view?usp=sharing"
+              href="https://drive.google.com/file/d/1sJAhFbsY10aZEqbXGmsPP5rTSVJbXUKL/view?usp=sharing"
               class="button"
             >
               Download My CV
